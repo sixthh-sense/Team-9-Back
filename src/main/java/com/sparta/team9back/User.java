424215c021af1e5829dcb439f56c9b7f338f9c42
HundredCleanWater <maxmp9904@gmail.com>
2021-12-13 16:56:14 +0900

    Getter

--- src/main/java/com/sparta/team9back/User.java
+++ src/main/java/com/sparta/team9back/User.java
@@ -1,10 +1,13 @@
 package com.sparta.team9back;
 
+import lombok.Getter;
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Getter
 public class User {
 
     @Id
