bedecea0949fda85638d425cbdbfa8d6114d0082
good4y <kmosh1212@naver.com>
2021-12-13 16:12:55 +0900

    first commit

---
+++ src/main/java/com/sparta/team9back/Team9BackApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.team9back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team9BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team9BackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/team9back/Team9BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.team9back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team9BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
